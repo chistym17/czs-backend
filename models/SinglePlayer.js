f75409e8ebcadf860b511ee482a401f7403ec285
chistym17 <mchisty31@gmail.com>
2025-04-30 23:15:54 +0600

    middlewares and single player registration added

---
+++ models/SinglePlayer.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const playerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    image: {
+        type: String,
+        required: false
+    },
+    batch: {
+        type: String,
+        required: true
+    },
+    position: {
+        type: String,
+        required: true
+    },
+
+});
+
+module.exports = mongoose.model('Player', playerSchema);
